85f80f14087d6fee0ba4e142012e5f400008c2da
Aldair Pedro <greatestattackturtle@gmail.com>
2022-09-09 09:38:31 -0400

    Create playRPSRound function to simulate a round of rock paper scissors

--- script.js
+++ script.js
@@ -14,9 +14,37 @@ function getComputerChoice(){
         case 6:
         case 7: 
         case 8:
-            return "Scissor";
+            return "Scissors";
             break;
         case 9:
             return getComputerChoice();
+    }
+}
+
+function playRPSRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection == computerSelection.toLowerCase()) {
+        return "It's a draw"
+    } else {
+        switch(playerSelection) {
+            case "rock":
+                if (computerSelection == "Paper") {
+                    return "You lost! Paper beats Rock."
+                } else {
+                    return "You win! Rock beats Scissors."
+                }
+            case "paper":
+                if (computerSelection == "Scissors") {
+                    return "You lost! Scissors beats Paper."
+                } else {
+                    return "You win! Paper beats Rock."
+                }
+            case "scissors":
+                if (computerSelection == "Rock") {
+                    return "You lost! Rock beats Scissors."
+                } else {
+                    return "You win! Scissors beats Paper."
+                }
+        }
     }
 }
